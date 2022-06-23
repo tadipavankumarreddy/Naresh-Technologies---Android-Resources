dabeeacba5ea7bbd3e982795ede720ddc739ab2a
PAVAN KUMAR <tadipavankumarreddy@gmail.com>
2022-06-23 20:24:00 +0530

    Project done

---
+++ FavoriteMovies/app/src/main/java/com/nareshittechnologies/favoritemovies/FavoriteMoviesAdapter.java
@@ -0,0 +1,66 @@
+package com.nareshittechnologies.favoritemovies;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+// FavoriteMoviesAdapter - is the RecyclerView Adapter class
+// For your adapter class- you need to provide the ViewHolder class info
+public class FavoriteMoviesAdapter extends RecyclerView.Adapter<FavoriteMoviesAdapter.FavoriteViewHolder> {
+
+    // to get the data to the adapter class - let us use the constructor
+    private Context context;
+    private int[] images;
+    private String[] names,actors;
+
+    public FavoriteMoviesAdapter(Context context, int[] images, String[] names, String[] actors) {
+        this.context = context;
+        this.images = images;
+        this.names = names;
+        this.actors = actors;
+    }
+
+    // onCreateViewHolder is responsible to attach the layout to the recyclerview.
+    @NonNull
+    @Override
+    public FavoriteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.one_item_design,parent,false);
+        return new FavoriteViewHolder(v);
+    }
+
+    // onBindViewholder will populate the data on the layouts attached to the Recyclerview
+    // based on the position of the layout
+    @Override
+    public void onBindViewHolder(@NonNull FavoriteViewHolder holder, int position) {
+        holder.imageView.setImageResource(images[position]);
+        holder.mName.setText(names[position]);
+        holder.mActor.setText(actors[position]);
+    }
+
+    // getItemCount() returns the total number of items on the Recyclerview
+    @Override
+    public int getItemCount() {
+        return images.length;
+    }
+
+    // Records the view information
+    public class FavoriteViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        TextView mName, mActor;
+
+        public FavoriteViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.poster);
+            mName = itemView.findViewById(R.id.movie_name);
+            mActor = itemView.findViewById(R.id.actor_name);
+
+        }
+    }
+}
--- FavoriteMovies/app/src/main/java/com/nareshittechnologies/favoritemovies/MainActivity.java
+++ FavoriteMovies/app/src/main/java/com/nareshittechnologies/favoritemovies/MainActivity.java
@@ -1,19 +1,46 @@
 package com.nareshittechnologies.favoritemovies;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private int[] movie_posters;
+    private String[] movie_names, actor_names;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         recyclerView = findViewById(R.id.recyclervieww);
+        createData();
+
+        FavoriteMoviesAdapter fma = new FavoriteMoviesAdapter(this,movie_posters,movie_names,actor_names);
+        recyclerView.setAdapter(fma);
+        // recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false));
+        // recyclerView.setLayoutManager(new GridLayoutManager(this,2));
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL));
+    }
+
+
+    // This method is responsible for preparing the data
+    private void createData() {
+        movie_posters = new int[]{R.drawable.avengers,R.drawable.bangbang,R.drawable.chhichore,
+        R.drawable.dangal,R.drawable.englishvinglish,R.drawable.fastoffury,R.drawable.gangster,
+        R.drawable.humsafar,R.drawable.indianajones,R.drawable.joker};
+
+        movie_names = new String[]{"Avengers","Bang Bang","Chhichore","Dangal","English Vinglish","Fast of Fury",
+        "Gangster","Humsafar","Indiana Jones","Joker"};
+
+        actor_names = new String[]{"Robert downey Jr","Hritik Roshan","Sushaant sing","Aamir Khan","Sridevi",
+        "Bruce lee","Emraan Hashmi","Jeethendra","Harrison Ford","Joaquin Phoeinx"};
     }
 }
 
@@ -29,3 +56,15 @@ public class MainActivity extends AppCompatActivity {
 // TODO 3: Add Recyclerview to the activity_main.xml, connect recyclerview to the obj
 
 // TODO 4: Define how you want to style each item on the recyclerview.
+
+// TODO 5: Set up data
+
+// TODO 6: Create an Adapter (pass ViewHolder info)
+//  the work of the adapter is what ?
+//  1. Take the data
+//  2. Populate the data on top of the one_row_design.xml file (imageview, textviews)
+//  3. Attach this layout (on which the data is there) to the Recyclerview
+
+// TODO 7: Create an object for the Adapter class and set the adapter on the Recyclerview
+
+// TODO 8: Set the layout Manager of your interest on Recyclerview
