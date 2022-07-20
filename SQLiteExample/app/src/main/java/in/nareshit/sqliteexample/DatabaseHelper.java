a647f044c0fcf4823effe3c502115c8d1d96055a
Pavan Kumar <tadipavankumarreddy@gmail.com>
2022-07-20 20:20:23 +0530

    SQLite

---
+++ SQLiteExample/app/src/main/java/in/nareshit/sqliteexample/DatabaseHelper.java
@@ -0,0 +1,71 @@
+package in.nareshit.sqliteexample;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    Context context;
+    // CREATING STATIC CONSTANTS FOR DATABASE_NAME, DATABASE_VERSION and also the TABLE_NAME, TABLE_ROWS
+    public static final String DATABASE_NAME = "pavan.db";
+    public static final int DATABASE_VERSION = 1;
+    public static final String TABLE_NAME = "students_data";
+    public static final String COL_0 = "student_id";
+    public static final String COL_1 = "student_name";
+    public static final String COL_2 = "student_age";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
+    }
+
+    // This method is used to create the tables in the database
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String CREATE_TABLE = "CREATE table "+TABLE_NAME+"("+COL_0+" INTEGER PRIMARY KEY AUTOINCREMENT, "+COL_1+" TEXT,"+COL_2+" INTEGER);";
+        sqLiteDatabase.execSQL(CREATE_TABLE);
+        Toast.makeText(context, "TABLE CREATED SUCCESSFULLY!", Toast.LENGTH_SHORT).show();
+
+    }
+
+    // this method is useful to upgrade the database tables
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE "+TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+
+    // Helper method to insert data
+    public void insertDatatoDB(ContentValues contentValues){
+        SQLiteDatabase db = getWritableDatabase();
+
+        db.insert(TABLE_NAME,null,contentValues);
+        Toast.makeText(context, "INSERTION IS SUCCESSFUL", Toast.LENGTH_SHORT).show();
+    }
+
+    // This is the helper method to fetch the data out of the database
+    public Cursor getData(){
+        SQLiteDatabase db = getReadableDatabase();
+        return db.rawQuery("SELECT * FROM "+TABLE_NAME, null);
+    }
+    
+    // this is helper method for update
+    public void updateData(ContentValues values, int id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.update(TABLE_NAME,values,COL_0+"="+id,null);
+        Toast.makeText(context, "DATA is updated", Toast.LENGTH_SHORT).show();
+    }
+    
+    // This is the helper method for delete
+    public void delete(int id){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("DELETE FROM "+TABLE_NAME+" where "+COL_0+"="+id);
+        Toast.makeText(context, "Data is removed", Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ SQLiteExample/app/src/main/java/in/nareshit/sqliteexample/MainActivity.java
@@ -0,0 +1,91 @@
+package in.nareshit.sqliteexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText id,name,age;
+    private TextView result;
+    private DatabaseHelper dbHelper;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+        // Initialize the dbhelper object
+        dbHelper = new DatabaseHelper(this);
+
+    }
+
+    // Insert
+    public void insertData(View view) {
+        String n = name.getText().toString();
+        int a = Integer.parseInt(age.getText().toString());
+        // Create ContentValues(...) object
+        ContentValues values = new ContentValues();
+        values.put(DatabaseHelper.COL_1, n);
+        values.put(DatabaseHelper.COL_2, a);
+        dbHelper.insertDatatoDB(values);
+    }
+
+    // READ
+    public void fetchData(View view) {
+       result.setText("");
+        Cursor c = dbHelper.getData();
+        if(c!=null){
+            c.moveToFirst();
+            do{
+                int s_id = c.getInt(0);
+                String s_n = c.getString(1);
+                int s_a = c.getInt(2);
+                result.append(s_id+" "+s_n+" "+s_a+"\n");
+            }while(c.moveToNext());
+        }
+    }
+
+    // UPDATE
+    public void updateData(View view) {
+        int s_id = Integer.parseInt(id.getText().toString());
+        String n = name.getText().toString();
+       String aaa = age.getText().toString();
+       int age2 = 0;
+       if(!aaa.equalsIgnoreCase("")){
+           age2 = Integer.parseInt(aaa);
+       }
+        // Create ContentValues(...) object
+        ContentValues values = new ContentValues();
+        values.put(DatabaseHelper.COL_1, n);
+        values.put(DatabaseHelper.COL_2, age2);
+        dbHelper.updateData(values,s_id);
+    }
+
+    // DELETE
+    public void deleteData(View view) {
+        int s_id = Integer.parseInt(id.getText().toString());
+        dbHelper.delete(s_id);
+    }
+
+    // This method is used to connect the views
+    private void initViews() {
+        id = findViewById(R.id.student_id);
+        name = findViewById(R.id.student_name);
+        age = findViewById(R.id.student_age);
+        result = findViewById(R.id.result);
+    }
+}
+
+// TODO 1: Create a DatabaseHelper class that extends to SQLiteOpenHelper
+// TODO 2: Override onCreate() and onUpgrade() methods of SQLiteOpenHelper
+// TODO 3: create a constructor matching the super class
+// TODO 4: Create students_data table in the onCreate(...) method of DatabaseHelper
+// TODO 5: create ContentValues object and insert data
+//  TODO 6: Fetch data
+//  TODO 7: update data
+//  TODO 8" Delete Data
